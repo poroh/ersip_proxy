215d4a498f6e09001a57a5c51c7293f7f663726c
Dmitry Poroh <poroh1981@yandex.ru>
2018-04-22 20:57:09 +0300

    Response passing is added

--- apps/ersip_proxy/src/ersip_proxy_conn.erl
+++ apps/ersip_proxy/src/ersip_proxy_conn.erl
@@ -8,7 +8,8 @@
 
 -module(ersip_proxy_conn).
 
--export([send/2,
+-export([send_request/2,
+         send_response/3,
          conn_init/1,
          conn_parse/3,
          conn_encode/3,
@@ -21,17 +22,46 @@
 %%%===================================================================
 
 
-send(NextURI, SipMsg) ->
+send_request(NextURI, SipMsg) ->
     [{host, Host}, {port, Port}] = ersip_uri:get([host, port], NextURI),
     HostBin = iolist_to_binary(ersip_host:assemble(Host)),
-    PortBin = integer_to_binary(Port),
+    PortBin =
+        case Port of
+            undefined ->
+                integer_to_binary(5060);
+            _ ->
+                integer_to_binary(Port)
+        end,
     TransportBin =
         case ersip_uri:params(NextURI) of
             #{transport := Transport} ->
-                ersip_transport:to_binary(Transport);
+                ersip_transport:assemble(Transport);
             _ ->
                 <<"udp">>
         end,
+    URI = <<"<ersip:", HostBin/binary, ":", PortBin/binary, ";transport=", TransportBin/binary, ">">>,
+    {ok, _Pid} =
+        nkpacket:send(URI,
+                      SipMsg,
+                      #{udp_to_tcp => false,
+                        udp_max_size => 9000,
+                        class => ersip
+                       }).
+
+send_response(TargetVia, _RecvVia, SipMsg) ->
+    Target =
+        case ersip_response:target(TargetVia) of
+            {reuse, FallbackTarget} ->
+                lager:warning("Connection reuse now is not supported yet"),
+                FallbackTarget;
+            {direct, DirectTarget} ->
+                DirectTarget
+        end,
+    lager:info("Sending response to ~p", [Target]),
+    {Host, Port, Transport, _Opts} = Target,
+    HostBin = iolist_to_binary(ersip_host:assemble(Host)),
+    PortBin = integer_to_binary(Port),
+    TransportBin = ersip_transport:assemble(Transport),
     URI = <<"<ersip:", HostBin/binary, ":", PortBin/binary, ";transport=", TransportBin/binary, ">">>,
     {ok, _Pid} =
         nkpacket:send(URI,
@@ -56,9 +86,9 @@ conn_init(NkPort) ->
     {ok, SIPConn}.
 
 conn_parse(Data, _NkPort, SIPConn) ->
-    {NextSIPConn, SE} = ersip_conn:conn_data(Data, SIPConn),
-    lists:foreach(fun process_side_effect/1, SE),
-    case need_disconnect(SE) of
+    {NextSIPConn, SEList} = ersip_conn:conn_data(Data, SIPConn),
+    lists:foreach(fun process_side_effect/1, SEList),
+    case need_disconnect(SEList) of
         true ->
             {stop, normal, NextSIPConn};
         false ->
@@ -69,7 +99,13 @@ conn_encode(SipMsg, _NkPort, SIPConn) ->
     SIPConn1 = SIPConn,
     Branch = ersip_proxy_stateless:branch(SipMsg),
     RawMsg  = ersip_sipmsg:raw_message(SipMsg),
-    RawMsg1 = ersip_conn:add_via(RawMsg, Branch, SIPConn),
+    RawMsg1 =
+        case ersip_msg:get(type, RawMsg) of
+            request ->
+                ersip_conn:add_via(RawMsg, Branch, SIPConn);
+            response ->
+                RawMsg
+        end,
     BinMSG = ersip_msg:serialize_bin(RawMsg1),
     lager:info("Sending message: ~n~s~n", [BinMSG]),
     {ok, BinMSG, SIPConn1}.
@@ -80,9 +116,12 @@ conn_handle_call(Request, _From, _NkPort, SIPConn) ->
 
 process_side_effect({bad_message, Data, Reason}) ->
     lager:warning("Bad message received: ~p: ~p", [Reason, Data]);
-process_side_effect({new_message, RawMsg}) ->
-    lager:info("New message received: ~n~s~n", [ersip_msg:serialize(RawMsg)]),
-    ersip_proxy_dispatcher:new_message(RawMsg);
+process_side_effect({new_request, RawMsg}) ->
+    lager:info("New request received: ~n~s~n", [ersip_msg:serialize(RawMsg)]),
+    ersip_proxy_dispatcher:new_request(RawMsg);
+process_side_effect({new_response, Via, RawMsg}) ->
+    lager:info("New response received: ~n~s~n", [ersip_msg:serialize(RawMsg)]),
+    ersip_proxy_dispatcher:new_response(Via, RawMsg);
 process_side_effect({disconnect, Error}) ->
     lager:info("Connection disconnect required: ~p", [Error]).
 
--- apps/ersip_proxy/src/ersip_proxy_dispatcher.erl
+++ apps/ersip_proxy/src/ersip_proxy_dispatcher.erl
@@ -8,20 +8,19 @@
 
 -module(ersip_proxy_dispatcher).
 
--export([new_message/1]).
-
+-export([new_request/1,
+         new_response/2]).
 
 %%====================================================================
 %% API functions
 %%====================================================================
 
-new_message(Message) ->
-    case ersip_msg:get(type, Message) of
-        request ->
-            process_request(Message);
-        response ->
-            process_response(Message)
-    end.
+new_request(Message) ->
+    process_request(Message).
+
+
+new_response(RecvVia, Message) ->
+    process_response(RecvVia, Message).
 
 
 %%====================================================================
@@ -31,28 +30,47 @@ new_message(Message) ->
 process_request(Message) ->
     case processing_type(Message) of
         {stateless, ProxyOptions} ->
-            processing_stateless(Message, ProxyOptions)
+            stateless_request(Message, ProxyOptions)
     end.
 
-process_response(_Message) ->
-    ok.
+process_response(RecvVia, Message) ->
+    stateless_response(RecvVia, Message).
 
 processing_type(_Message) ->
-    {stateless, #{}}.
+    RR = ersip_uri:make(<<"sip:192.168.100.11:5090">>),
+    {stateless,
+     #{to_tag => ersip_id:token(crypto:strong_rand_bytes(7)),
+       record_route_uri => RR,
+       check_rroute_fun => fun(X) -> ersip_uri:make_key(X) == ersip_uri:make_key(RR) end
+      }}.
 
-processing_stateless(Message, ProxyOptions) ->
+stateless_request(Message, ProxyOptions) ->
     case ersip_proxy_common:request_validation(Message, ProxyOptions) of
         {ok, SipMsg} ->
             SipMsg1 = ersip_proxy_common:process_route_info(SipMsg, ProxyOptions),
             Target = stateless_target(SipMsg1),
             lager:info("Forward message to target: ~s", [ersip_uri:assemble(Target)]),
             {SipMsg2, #{nexthop := NexthopURI}} = ersip_proxy_common:forward_request(Target, SipMsg1, ProxyOptions),
-            ersip_proxy_conn:send(NexthopURI, SipMsg2);
-        {reply, SipMsg} ->
-            lager:info("Message reply ~p", [SipMsg]);
+            lager:info("Nexthop is: ~s", [ersip_uri:assemble(NexthopURI)]),
+            ersip_proxy_conn:send_request(NexthopURI, SipMsg2);
+        {reply, SipMsg2} ->
+            lager:info("Message reply ~p", [SipMsg2]),
+            TargetVia = ersip_sipmsg:get(topmost_via, SipMsg2),
+            spawn_link(fun() ->
+                               ersip_proxy_conn:send_response(TargetVia, undefined, SipMsg2)
+                       end);
         {error, Reason} ->
             lager:warning("Error occured during processing: ~p", [Reason])
     end.
 
-stateless_target(_SipMsg) ->
-    ersip_uri:make(<<"sip:192.168.100.11:5060">>).
+stateless_response(RecvVia, Message) ->
+    case ersip_proxy_stateless:process_response(RecvVia, Message) of
+        {forward, TargetVia, SipMsg} ->
+            lager:info("Forward response to ~p", [TargetVia]),
+            ersip_proxy_conn:send_response(TargetVia, RecvVia, SipMsg);
+        {drop, Reason} ->
+            lager:warning("Cannot forward message: ~p", [Reason])
+    end.
+
+stateless_target(SipMsg) ->
+    ersip_sipmsg:ruri(SipMsg).
